da9825ec0ee5c801643cf5228974bb74f697204a
Muhaimin Alrubaie <80754795+MohimnMajid@users.noreply.github.com>
2024-12-16 19:12:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,230 @@
+const menu = [
+  {
+    id: 1,
+    title: "شاورما دجاج حجري",
+    category: "دجاج",
+    price: "4,000",
+    img: "https://scontent.fbgw41-3.fna.fbcdn.net/v/t1.6435-9/78688528_2732929300105907_8479570644210548736_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGzKgImVddBkrqkZh8BXrcB9EhBUEdSNRL0SEFQR1I1EmBEDofz36BpeuACCFDqvj78q4lNAbgo8A4RUEstPTs3&_nc_ohc=q-GyyFHiAnAQ7kNvgF3mvmh&_nc_zt=23&_nc_ht=scontent.fbgw41-3.fna&_nc_gid=AOsbxvhFT3OP7qY0kckZKHg&oh=00_AYAVaC36yH9Cf7Xsh2N5fQz5493qKxnYU8ftIQgLr0aHRQ&oe=677A6929",
+    desc: "شاورما دجاج بالصمون الحجري",
+  },
+  {
+    id: 2,
+    title: "شاورما دجاج فرنجي",
+    category: "دجاج",
+    price: "4,000",
+    img: "https://scontent.fbgw41-1.fna.fbcdn.net/v/t1.6435-9/197830219_284871593377640_38858266213686140_n.jpg?stp=dst-jpg_p552x414_tt6&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFXTvg0n0rQU1ztOuDIsgOm_uTbGOj7svr-5NsY6Puy-jC72pBhRiwJMi2V28GUdypmEcqVIVg5mqJPhC0yB-3o&_nc_ohc=lx_eTs8VHJwQ7kNvgE4XLHZ&_nc_zt=23&_nc_ht=scontent.fbgw41-1.fna&_nc_gid=AbGq77Dr3-3XoipHfOdoIhF&oh=00_AYA68nX2pDYkc1FO3NI9NWZhGCpLZh7mUW4lqkscFu5l4g&oe=677A6D2C",
+    desc: "شاورما دجاج فرنجي",
+  },
+  {
+    id: 3,
+    title: "شاورما دجاج ابو العبد",
+    category: "دجاج",
+    price: "4,000",
+    img: "https://aseertime.com/wp-content/uploads/2021/06/4-5-scaled.jpg",
+    desc: "شاورما دجاج بطريقة ابو العبد",
+  },
+  {
+    id: 4,
+    title: "وجبة شراك دجاج مع مشروب غازي",
+    category: "دجاج",
+    price: "4,500",
+    img: "https://images.deliveryhero.io/image/hungerstation/menus/menuitem/hsimg-7842813?width=1440&quality=75",
+    desc: "سندويتش شاورما دجاج مع مشروب غازي من إختيارك",
+  },
+  {
+    id: 5,
+    title: "وجبة شاورما دجاج مع مشروب غازي",
+    category: "دجاج",
+    price: "4,500",
+    img: "https://lalafia.com/uploads/products/9vyFN8CgbzWgD9PDSBMplYHhkCWdRliVA9CKfquU.jpg",
+    desc: "سندويتش شاورما دجاج مع مشروب غازي من إختيارك",
+  },
+  {
+    id: 6,
+    title: "وجبة دجاج عائلي مع مشروب غازي",
+    category: "دجاج",
+    price: "12,000",
+    img: "https://scontent.fbgw41-3.fna.fbcdn.net/v/t39.30808-6/347409431_653193056832008_2213733634320761098_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEvgHIUHzlGMGLe6GZlsIhloD4DorOmFZegPgOis6YVl_KuQm4c4CrmYFYyu0BvLKrsv-ETnd8mlsq98xbiOoc3&_nc_ohc=kX9EqcoF7EoQ7kNvgEuo_rQ&_nc_zt=23&_nc_ht=scontent.fbgw41-3.fna&_nc_gid=AW5RNGfqrcE8_UBTGs-tdpM&oh=00_AYC2glYjM2mCasMhROahEIpvRsVHQ1B8aeSWngiNwd7i7A&oe=6758B0E5",
+    desc: "وجبة دجاج عائلية تكفي ل 4 أشخاص",
+  },
+  {
+    id: 7,
+    title: "تمن ابو العبد دجاج",
+    category: "دجاج",
+    price: "4,500",
+    img: "https://alzawraapaper.com/vrsfls/cntnt/pctr/101158.jpg",
+    desc: "ريزو دجاج",
+  },
+  {
+    id: 8,
+    title: "طبق شاورما دجاج 200 غم",
+    category: "دجاج",
+    price: "6,000",
+    img: "https://scontent.fbgw41-2.fna.fbcdn.net/v/t39.30808-6/274956475_456162282915236_3981742065107908354_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeG4Q3Xlch9fnzCXtNVcaiymTWGjbaXOHUVNYaNtpc4dRSgtp_7ylG4qxDv7gsM6Q14grmnGpPWbzLR9fP6xXpLI&_nc_ohc=RZ343foImB4Q7kNvgG1Vswu&_nc_zt=23&_nc_ht=scontent.fbgw41-2.fna&_nc_gid=A5wvEIBVY3rv_Vri0calYEO&oh=00_AYAdJXO_YB-hgBB9bUwDJ7UP_TiVc7Lw-P1cSDmMrISyEA&oe=6758DAF5",
+    desc: "شاورما دجاج وزن 200 غم في صحن",
+  },
+  {
+    id: 9,
+    title: "نصف كيلو شاورما دجاج",
+    category: "دجاج",
+    price: "13,000",
+    img: "https://www.elwekala-tm.com/uploads/products/ZgqCJk5OBYLwrL5XOVIwQtfnaAslnRX62xqueiqf.jpg",
+    desc: "500 غرام شاورما دجاج",
+  },
+  {
+    id: 10,
+    title: "شاورما لحم حجري",
+    category: "لحم",
+    price: "4,500",
+    img: "https://scontent.fbgw41-3.fna.fbcdn.net/v/t1.6435-9/78431742_2732929293439241_5174016302557167616_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFNItEkARzgbmqVPPHq7IVuiKaJzwLlcc2IponPAuVxzaYuzOghvlibTF85o8qHetbvK6WsJBd47nblqfYZ1HDh&_nc_ohc=D2iM6AourBQQ7kNvgHPMDaO&_nc_zt=23&_nc_ht=scontent.fbgw41-3.fna&_nc_gid=AM0J7QGqeVqVab4T5sbUwl3&oh=00_AYBAOr1k--1i5MSwCmSDqc9Z_Tp09Ke4Kiov2n_VDorkHg&oe=677A51E9",
+    desc: "شاورما لحم بالصمون الحجري",
+  },
+  {
+    id: 11,
+    title: "شاورما لحم فرنجي",
+    category: "لحم",
+    price: "4,500",
+    img: "https://iqmenus.com/Images/K8eOtOvWD9dwuwgJ.jpg",
+    desc: "شاورما لحم فرنجي",
+  },
+  {
+    id: 12,
+    title: "شاورما لحم",
+    category: "لحم",
+    price: "3,500",
+    img: "https://shamlola.s3.amazonaws.com/Shamlola_Images/9/src/0ee779ba72d9c33b32056333a9d87c4f81f10f15.jpg",
+    desc: "شاورما لحم تقليدية",
+  },
+  {
+    id: 13,
+    title: "وجبة شراك لحم مع مشروب غازي",
+    category: "لحم",
+    price: "5,500",
+    img: "https://modo3.com/thumbs/fit630x300/138145/1531832434/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D8%B4%D8%A7%D9%88%D8%B1%D9%85%D8%A7_%D8%A7%D9%84%D9%84%D8%AD%D9%85_%D8%A7%D9%84%D9%85%D8%B5%D8%B1%D9%8A%D8%A9.jpg",
+    desc: "وجبة شراك تأتي مع مشروب غازي من إختيارك",
+  },
+  {
+    id: 14,
+    title: "وجبة مكس لحم ودجاج",
+    category: "لحم&دجاج",
+    price: "15,000",
+    img: "https://scontent.fbgw41-3.fna.fbcdn.net/v/t39.30808-6/274004175_447069320491199_1637860589492732660_n.jpg?stp=dst-jpg_p552x414_tt6&_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHdulnjcCV0zmb6CPHQ8eHKBTjy1Nh78NUFOPLU2Hvw1VRG7D9BnR37sRnBtZ4czuuYQsfXAKBvrSqXpEp8D8_0&_nc_ohc=uyjJ-AKkyUEQ7kNvgHTyYyG&_nc_zt=23&_nc_ht=scontent.fbgw41-3.fna&_nc_gid=AnpzAUgeS_dX-wyAHfNneWz&oh=00_AYAO_deiTcloT1E08WU7uKe0nwSUZZWReHRA3iYcqdJxnA&oe=6758D716",
+    desc: "وجبة شاورما دجاج ولحم معاً",
+  },
+  {
+    id: 15,
+    title: "وجبة شاورما لحم مع مشروب غازي",
+    category: "لحم",
+    price: "5,500",
+    img: "https://images.deliveryhero.io/image/menu-import-gateway-prd/regions/ME/chains/HS-SNTION/da03ad4a3f5d4a6963282cf64f52957e.jpg?width=1440&quality=75",
+    desc: "شاورما لحم مع مشروب غازي",
+  },
+  {
+    id: 16,
+    title: "وجبة لحم عائلي مع مشروب غازي",
+    category: "لحم",
+    price: "16,500",
+    img: "https://panel.3roodcom.com/content_data/items/2023/6/m1gmvxzhzmob6wzk61pk6jwf.jpg",
+    desc: "وجبة لحم تكفي 4 أشخاص مع 4 مشروبات غازية",
+  },
+  {
+    id: 17,
+    title: "تمن ابو العبد لحم",
+    category: "لحم",
+    price: "5,500",
+    img: "https://scontent.fbgw41-2.fna.fbcdn.net/v/t1.6435-9/51014121_2823003424590857_4759763154920013824_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHHPpAVvA6D-W7c8-gewbZQXsAJKpgX2tdewAkqmBfa1zPXLVbKGnPo5-FRCWbfyYNy0F1vYtdnZn4tGdKD-lqA&_nc_ohc=42fwtHGfXS8Q7kNvgGk4n6j&_nc_zt=23&_nc_ht=scontent.fbgw41-2.fna&_nc_gid=A6fP5eFsSYtW7jBTeBC3EcX&oh=00_AYBMw5usOhcc_xW85IYGpp3eOpnJsbY-Sgr5n7BhccUIdg&oe=677A4400",
+    desc: "ريزو شاورما لحم",
+  },
+  {
+    id: 18,
+    title: "طبق شاورما لحم 200 غم",
+    category: "لحم",
+    price: "8,000",
+    img: "https://static.aljamila.com/styles/1100x732/public/8a4488c177d9dc8c3da7c745c89ca214.jpg?h=ccf9ec54",
+    desc: "شاورما لحم 200 غرام وزن",
+  },
+  {
+    id: 19,
+    title: "نصف كيلو شاورما لحم",
+    category: "لحم",
+    price: "17,000",
+    img: "https://scontent.fbgw41-4.fna.fbcdn.net/v/t39.30808-6/462424416_2484653481727519_5903119347363050884_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=0b6b33&_nc_eui2=AeHjlFcoILaU6jqDuJRRwBAnSEg0INP5CzJISDQg0_kLMnyjn_A8NjXrjVSrP3f0uTkCFZbW1vZXC6fx_tY9Z4oq&_nc_ohc=6er16TMLWsgQ7kNvgEcVUIX&_nc_zt=23&_nc_ht=scontent.fbgw41-4.fna&_nc_gid=A-Dm_2Ux5bhORXtu666Zja6&oh=00_AYBsMOTiOqxez1CWFcMlC-WTxI-bzcDnZvAeDJJnEyejRw&oe=6758DD64",
+    desc: "500 غرام شاورما لحم",
+  },
+  {
+    id: 20,
+    title: "كيلو شاورما لحم",
+    category: "لحم",
+    price: "34,000",
+    img: "https://scontent.fbgw41-4.fna.fbcdn.net/v/t39.30808-6/462424416_2484653481727519_5903119347363050884_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=0b6b33&_nc_eui2=AeHjlFcoILaU6jqDuJRRwBAnSEg0INP5CzJISDQg0_kLMnyjn_A8NjXrjVSrP3f0uTkCFZbW1vZXC6fx_tY9Z4oq&_nc_ohc=6er16TMLWsgQ7kNvgEcVUIX&_nc_zt=23&_nc_ht=scontent.fbgw41-4.fna&_nc_gid=A-Dm_2Ux5bhORXtu666Zja6&oh=00_AYBsMOTiOqxez1CWFcMlC-WTxI-bzcDnZvAeDJJnEyejRw&oe=6758DD64",
+    desc: "1 كيلو شاورما لحم",
+  },
+];
+const section = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+const categories = menu.reduce(
+(values, item) => {
+  if (!values.includes(item.category)) {
+    values.push(item.category);
+  }
+  return values;
+},
+["الكل"]
+);
+
+const categoryList = () => {
+const categoryBtns = categories
+  .map((category) => {
+    return `<button class="btn btn-outline-dark btn-item" data-id=${category}>${category}</button>`;
+  })
+  .join("");
+
+btnContainer.innerHTML = categoryBtns;
+const filterBtns = document.querySelectorAll(".btn-item");
+
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const category = e.currentTarget.dataset.id;
+    console.log(category);
+    const menuCategory = menu.filter((menuItem) => {
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    if (category === "الكل") {
+      menuList(menu);
+    } else {
+      menuList(menuCategory);
+    }
+  });
+});
+};
+
+const menuList = (menuItems) => {
+let displayMenu = menuItems.map((item) => {
+  return `<div class="menu-items col-lg-6 col-sm-12">
+          <img
+            src=${item.img}
+            alt=${item.title}
+            class="photo"
+          />
+          <div class="menu-info">
+            <div class="menu-title">
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}</h4>
+            </div>
+            <div class="menu-text">
+              ${item.desc}
+            </div>
+          </div>
+        </div>
+  `;
+});
+displayMenu = displayMenu.join("");
+section.innerHTML = displayMenu;
+};
+
+menuList(menu);
+categoryList();
